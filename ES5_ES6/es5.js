a88f6c3ab97b7cebb40d72dcf3e31d5c55fc82db
Rachna-11 <jariyarachna11@gmail.com>
2025-01-01 18:31:50 +0530

    first commit

---
+++ ES5_ES6/es5.js
@@ -0,0 +1,18 @@
+// ES5
+
+function studInfo() // Create a function for student details 
+{
+    // create a variable
+
+    var stud_name = "Lara";
+    var stud_age = 18;
+    var passed = true;
+
+    if (passed) // check if the students passed
+    {
+        console.log("Student Name: ", stud_name); // print student name
+        console.log("Student Age: ", stud_age); // print student age   
+    }
+}
+
+studInfo(); // function call
